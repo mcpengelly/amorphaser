4cab2239011f03cf49c0df0e3576531b3e973bc8
mcpengelly <pengelly.mat@gmail.com>
2016-09-22 22:57:21 -0400

    cleanup comments and fix blade type

--- src/scripts/entity/enemy.ts
+++ src/scripts/entity/enemy.ts
@@ -11,9 +11,7 @@ module amorphaser.Entity {
 		constructor(game: Phaser.Game, x: number, y: number) {
 			super(game, x, y, 'enemy');
 			this.game = game;
-			//game.add.existing(this);
-
-			//this.body.collideWorldBounds = true;
+			this.body.collideWorldBounds = true;
 		}
 
 	}
